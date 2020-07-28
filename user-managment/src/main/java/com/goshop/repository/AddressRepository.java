8572d95d64a4611a0d457f405981fee4fb94df2c
mahmoud gomaa <gomaamahmoud922@gmail.com>
2020-07-28 17:17:38 -0500

    add database properties at user_management

--- user-managment/src/main/java/com/goshop/repository/AddressRepository.java
+++ user-managment/src/main/java/com/goshop/repository/AddressRepository.java
@@ -1,6 +1,9 @@
 package com.goshop.repository;
 
+import com.goshop.model.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
 }
