a9ecc4daf4fb23103be717637b98f4b54d59ab1f
AndrewThomasW <54901855+AndrewThomasW@users.noreply.github.com>
2020-07-31 15:51:00 -0500

    category changes
    category  changes

--- product-service/src/main/java/com/goshop/productservice/service/CategoryService.java
+++ product-service/src/main/java/com/goshop/productservice/service/CategoryService.java
@@ -1,5 +1,16 @@
 package com.goshop.productservice.service;
 
-public interface CategoryService {
+import com.goshop.productservice.exception.NoSuchResourceException;
+import com.goshop.productservice.model.Category;
+import org.springframework.http.ResponseEntity;
 
+import java.util.List;
+
+public interface CategoryService {
+    public  Category addCategory(Category category);
+    public Category getCategory(long categoryId) throws NoSuchResourceException;
+    public List<Category> getCategories();
+
+    public Category editCategory(long categoryId, Category edit_category);
+    public ResponseEntity<Void> deleteCategory(long categoryId) throws NoSuchResourceException;
 }
--- product-service/src/main/java/com/goshop/productservice/service/impl/CategoryServiceImpl.java
+++ product-service/src/main/java/com/goshop/productservice/service/impl/CategoryServiceImpl.java
@@ -1,17 +1,66 @@
 package com.goshop.productservice.service.impl;
 
+import com.goshop.productservice.exception.NoSuchResourceException;
+import com.goshop.productservice.model.Category;
 import com.goshop.productservice.repository.CategoryRepository;
 import com.goshop.productservice.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @Transactional(propagation = Propagation.REQUIRED)
 public class CategoryServiceImpl implements CategoryService {
-	
-	@Autowired
+
+
+    @Autowired
     CategoryRepository categoryRepository;
+
+    @Override
+    public Category addCategory(Category category) {
+
+        return categoryRepository.save(category);
+    }
+
+    @Override
+    public Category getCategory(long categoryId) throws NoSuchResourceException {
+        Category category = categoryRepository.findById(categoryId).orElseThrow(() ->
+                new NoSuchResourceException("No Category found  with" , categoryId));
+
+        return category;
+    }
+
+
+    @Override
+    public List<Category> getCategories() {
+
+        return categoryRepository.findAll();
+    }
+
+    @Override
+    public Category editCategory(long categoryId, Category edit_category) {
+
+        Category category = categoryRepository.findById(categoryId).orElseThrow(() ->
+                new  NoSuchResourceException("No category found  with" , categoryId));
+
+        category.setDescription(edit_category.getDescription());
+        category.setName(edit_category.getName());
+
+        return categoryRepository.save(category);
+    }
+
+    @Override
+    public ResponseEntity<Void> deleteCategory(long categoryId) throws NoSuchResourceException {
+        Category category = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new NoSuchResourceException("No category found  with", categoryId));
+
+        categoryRepository.delete(category);
+
+        return ResponseEntity.noContent().build();
+    }
 
 }
