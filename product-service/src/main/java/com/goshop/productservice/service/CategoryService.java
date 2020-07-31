9ed63e111c378e1bb46e05840adee4d56ac2aca3
AndrewThomasW <54901855+AndrewThomasW@users.noreply.github.com>
2020-07-31 16:58:42 -0500

    category changes
    category changes

--- product-service/src/main/java/com/goshop/productservice/service/CategoryService.java
+++ product-service/src/main/java/com/goshop/productservice/service/CategoryService.java
@@ -10,7 +10,6 @@ public interface CategoryService {
     public  Category addCategory(Category category);
     public Category getCategory(long categoryId) throws NoSuchResourceException;
     public List<Category> getCategories();
-
     public Category editCategory(long categoryId, Category edit_category);
     public ResponseEntity<Void> deleteCategory(long categoryId) throws NoSuchResourceException;
 }
