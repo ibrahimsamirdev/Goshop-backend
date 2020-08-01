852e931d39546ee116321036c881754489537a7a
Emad <eshehate@mum.edu>
2020-08-01 11:34:37 -0500

    Merge create order and orderDetails model

--- order-service/src/main/java/com/goshop/orderservice/model/Order.java
+++ order-service/src/main/java/com/goshop/orderservice/model/Order.java
@@ -1,12 +1,67 @@
 package com.goshop.orderservice.model;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
+@Table(name="orders")
 public class Order {
 
     @Id
     @GeneratedValue
     private long id;
+    private long userId;
+    private double totalAmount;
+    private long addressId;
+    private long paymentId;
+    @OneToMany(mappedBy ="order")
+    private List<OrderDetails> orderDetails;
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        id = id;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public double getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public long getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(long addressId) {
+        this.addressId = addressId;
+    }
+
+    public long getPaymentId() {
+        return paymentId;
+    }
+
+    public void setPaymentId(long paymentId) {
+        this.paymentId = paymentId;
+    }
+
+    public List<OrderDetails> getOrderDetails() {
+        return orderDetails;
+    }
+
+    public void setOrderDetails(List<OrderDetails> orderDetails) {
+        this.orderDetails = orderDetails;
+    }
 }
