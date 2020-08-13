127d554706295ba2fd13045e32539d9c4371f868
Ibrahim Samir <ibrahimsamirdev@gmail.com>
2020-08-13 02:46:53 -0500

    Products Management: Search Products

--- product-service/src/main/java/edu/miu/productservice/repository/ProductRepository.java
+++ product-service/src/main/java/edu/miu/productservice/repository/ProductRepository.java
@@ -1,47 +1,51 @@
 package edu.miu.productservice.repository;
 
-import edu.miu.productservice.model.Product;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import edu.miu.productservice.model.Product;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    @Query("SELECT d from Product d where d.vendorId= :id order by d.id")
-    List<Product> findRProductsByVendorId(@Param("id") Long id);
+	@Query("SELECT d from Product d where d.vendorId= :id order by d.id")
+	List<Product> findRProductsByVendorId(@Param("id") Long id);
 
-    @Query("SELECT d from Product d order by d.vendorId,d.id")
-    List<Product> findRProductsAdmin();
+	@Query("SELECT d from Product d order by d.vendorId,d.id")
+	List<Product> findRProductsAdmin();
 
-    //ANDREW: Advanced Search
+	// ANDREW: Advanced Search
 
-    //Simple derived query with parameters
-    List<Product> findByTitle(String title);
+	// Simple derived query with parameters
+	List<Product> findByTitle(String title);
 
-    //Derived queries with multiple parameters
-    List<Product> findByTitleAndDescription(String title, String description);
+	// Derived queries with multiple parameters
+	List<Product> findByTitleAndDescription(String title, String description);
 
-    //Traverse associations in derived queries
-     List<Product> findByCategoryName(String title);
+	// Traverse associations in derived queries
+	List<Product> findByCategoryName(String title);
 
-     List<Product> findByVendorIdAndIsDeletedFalse(long vendorId);
+	List<Product> findByVendorIdAndIsDeletedFalse(long vendorId);
 
-     List<Product> findProductByVendorIdAndIsPublishedTrueAndIsDeletedFalse(long vendorId);
+	List<Product> findProductByVendorIdAndIsPublishedTrueAndIsDeletedFalse(long vendorId);
 
-    List<Product> findProductByVendorIdAndIsPublishedNotAndIsDeletedFalse(long vendorId, boolean t);
+	List<Product> findProductByVendorIdAndIsPublishedNotAndIsDeletedFalse(long vendorId, boolean t);
 
+	List<Product> findProductByIsPublishedTrueAndIsDeletedFalse();
 
+	List<Product> findProductByIsPublishedNotAndIsDeletedFalse(boolean t);
 
-    List<Product> findProductByIsPublishedTrueAndIsDeletedFalse();
+	List<Product> findByIsDeletedNot(boolean t);
 
-    List<Product> findProductByIsPublishedNotAndIsDeletedFalse(boolean t);
+	List<Product> findByIsDeletedTrue();
 
-    List<Product> findByIsDeletedNot(boolean t);
-
-    List<Product> findByIsDeletedTrue();
+	@Query("SELECT p FROM Product p WHERE title LIKE %:keyword% "
+			+ "AND (:categoryid is null or p.category.id = :categoryid) "
+			+ "AND (:minprice is null or p.price >= :minprice) " + "AND (:maxprice is null or p.price <= :maxprice) ")
+	List<Product> searchProducts(String keyword, Long categoryid, Double minprice, Double maxprice);
 
 }
