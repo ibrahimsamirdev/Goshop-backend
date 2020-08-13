5477b96be711637969475e4670de37ec75d70385
Hedra <Hedra_gerges@live.com>
2020-08-13 15:35:38 -0500

    final edit

--- product-service/src/main/java/edu/miu/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/edu/miu/productservice/ProductServiceApplication.java
@@ -3,12 +3,12 @@ package edu.miu.productservice;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 
 public class ProductServiceApplication {
 
