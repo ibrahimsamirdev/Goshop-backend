c59dafcb5ebbd96c9bb6a6ff7d01d7d0ede9027c
Ibrahim Samir <ibrahimsamirdev@gmail.com>
2020-08-14 01:31:58 -0500

    Handel Duplicate Key Exception

--- user-managment/src/main/java/com/goshop/exception/GlobalExceptionHandlerController.java
+++ user-managment/src/main/java/com/goshop/exception/GlobalExceptionHandlerController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -35,4 +36,8 @@ public class GlobalExceptionHandlerController {
 		return new ResponseEntity<Object>(e.getMessage(), HttpStatus.BAD_REQUEST);
 	}
 
+	@ExceptionHandler(DataIntegrityViolationException.class)
+	public ResponseEntity<Object> handleDuplicateKeyException(DataIntegrityViolationException e) {
+		return new ResponseEntity<Object>("Entity Already Exists", HttpStatus.CONFLICT);
+	}
 }
